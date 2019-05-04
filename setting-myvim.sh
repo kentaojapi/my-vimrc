90080b0f02988f534d136507a7cdeb343bca2343
Kenta Ojapi <kentaojapi@gmail.com>
2019-05-04 22:29:53 +0900

    Create setting-myvim.sh

---
+++ setting-myvim.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+apt-get update
+apt-get install git
+apt-get install vim
+git clone https://github.com/kentaojapi/my-vimrc.git ~/my-vimrc
+sudo cp ~/my-vimrc/vimrc ~/.vimrc
